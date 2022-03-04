254f4092f00a596618543b158a6205f3a23c9cf3
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-03-04 12:20:01 -0300

    Implementação do use case Show User Profile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const foundUser = this.showUserProfileUseCase.execute({ user_id })
+
+      return response.status(200).json(foundUser);
+    } catch( err ) {
+      return response.status(404).json({err: err.message});
+    }
   }
 }
 
