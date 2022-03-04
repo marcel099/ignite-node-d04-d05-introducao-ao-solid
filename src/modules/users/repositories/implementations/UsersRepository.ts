c93ec6aa87a71cfe6572e48853bffebae3899914
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-03-04 11:44:54 -0300

    Implementação do model e repository User

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,47 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+      admin: false,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const foundUser = this.users.find((user) => user.id === id);
+
+    return foundUser;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const foundUser = this.users.find((user) => user.email === email);
+
+    return foundUser;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    receivedUser.admin = true;
+
+    const userIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+
+    this.users[userIndex] = receivedUser;
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
